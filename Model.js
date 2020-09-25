7f9a818c23420ec321240b557f2a7baa32543412
Parag Adhau <adhauparag64@gmail.com>
2020-09-25 17:40:17 +0530

    Added :Code if gambler win or loose 50% of stake then resign for a day

--- Model.js
+++ Model.js
@@ -5,20 +5,23 @@
         var winStake = 100;
         var lostStake = 100;
         var r1,r2;
-        while(day == 1){
+        while(winStake<150 || lostStake>50){
         var Random = Math.floor((Math.random()*10));
         r1= Random%10;
         r2=r1%2;
         if(r2 == 0){
-        lostStake = lostStake-1;
-        console.log("********Gambler Loose**********");
-        }
+            lostStake = lostStake-1;
+           }
         else{
             winStake = winStake+1;
-            console.log("********Gambler Win**********");
-        }
-        day = day+1;
+           }
        }
+       if(winStake==150){
+         console.log("Gambler win 50% of stake");
+         }
+         else{
+             console.log("Gambler loose 50% of stake");
+         }
      }
  }
  module.exports = new Model();
