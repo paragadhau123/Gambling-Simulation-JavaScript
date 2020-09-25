783d1755e0ccea43aa4d88d9d4e00e7ce1ded346
Parag Adhau <adhauparag64@gmail.com>
2020-09-25 20:20:38 +0530

    Added :Code for finding luckiest and unluckiest day in month

--- Model.js
+++ Model.js
@@ -3,7 +3,9 @@
         var stake = 100;
         var day = 1;
         var r1,r2;
-        
+        let array = [];
+        array.length = 30;
+        var dailyWin = 0;
         while(day<=30){
             var winStake = 100;
             var lostStake = 100;
@@ -15,13 +17,18 @@
         r2=r1%2;
         if(r2 == 0){
             lostStake = lostStake-1;
+            dailyWin = dailyWin - 1;
+            array[day-1]=dailyWin;
            }
         else{
             winStake = winStake+1;
+            dailyWin = dailyWin+1;
+            array[day-1]=dailyWin;
            }
        }
        winAmount=winStake-stake;
        lostAmount=stake-lostStake;
+       console.log("----------------------------------------------------------------------------------");
        console.log("Win amount of day"+day+" is :"+winAmount+" and Lost amount of day"+day+" is :"+lostAmount);
        if( winAmount > lostAmount ){
         console.log("Day"+day+" win by "+(winAmount-lostAmount));
@@ -31,7 +38,28 @@
         }
        day=day+1;
      }
-   }
+   
+    var luckyDay = 0;
+    var unluckyDay = 0;
+    var high = array[0];
+    var low = array[0];
+    for (var i = 1; i < array.length; i++)
+    {
+            if( high < array[i] )
+             {
+                    high = array[i];
+                    luckyDay = i+1;
+            }
+            if( low > array[i] )
+            {
+                    low = array[i];
+                    unluckyDay = i+1;
+            }
+
+        }
+        console.log("Luckiest day is day : "+luckyDay+" and He won : "+high);
+        console.log("Unluckiest day is day : "+unluckyDay+" and He lost : "+low);
+    }
 }
  module.exports = new Model();
  
