aa7b2544801da540cfcd4e21422df847e59e11af
Parag Adhau <adhauparag64@gmail.com>
2020-09-25 17:50:28 +0530

    Added :Code to calculate win and loose amount of per day

--- Model.js
+++ Model.js
@@ -2,9 +2,12 @@
      Simulation(){
         var stake = 100;
         var day = 1;
-        var winStake = 100;
-        var lostStake = 100;
         var r1,r2;
+        var winAmount;
+        var lostAmount;
+        while(day<=20){
+            var winStake = 100;
+            var lostStake = 100;
         while(winStake<150 || lostStake>50){
         var Random = Math.floor((Math.random()*10));
         r1= Random%10;
@@ -16,13 +19,12 @@
             winStake = winStake+1;
            }
        }
-       if(winStake==150){
-         console.log("Gambler win 50% of stake");
-         }
-         else{
-             console.log("Gambler loose 50% of stake");
-         }
+       winAmount=winStake-stake;
+       lostAmount=stake-lostStake;
+       console.log("Win amount of day"+day+" is :"+winAmount+" and Lost amount of day"+day+" is :"+lostAmount);
+       day=day+1;
      }
- }
+   }
+}
  module.exports = new Model();
  
